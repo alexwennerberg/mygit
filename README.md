7973f6e1e3ee1bc688d6c4bb262dff21509969c2
alex wennerberg <alex@alexwennerberg.com>
2021-03-09 22:17:01 -0800

    Add basaic index/repo pages

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Mygit -- the world's smallest Git host
+# mygit -- the world's smallest Git host
 
-https://public-inbox.org/README.html
+A small, self-hosted git forge, with a web viewer for repositories and and a mailing list archive
 
 ## Why Rust?
 
-There is IMO no compelling engineering reason to write this in Rust instead of Go. I'm doing so primarily as an exercise and as an opportunity to improve some of the Rust ecosystem.
+There is IMO no compelling engineering reason to write this in Rust instead of a language like Go. I'm doing so primarily as an exercise and as an opportunity to improve some of the Rust ecosystem.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use tide::Request;
+use askama::Template;
 use tide::prelude::*;
+use std::path::Path;
 use std::time::Instant;
 use once_cell::sync::OnceCell;
 use std::fs;
 use serde::{Serialize, Deserialize};
 use pico_args;
+use git2::Repository;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    port: i32 // should be u8
+    port: i32, // should be u8
+    repo_directory: String
 }
 
 static CONFIG: OnceCell<Config> = OnceCell::new();
@@ -20,9 +24,41 @@ impl Config {
 
 }
 
-async fn index(req: Request<()>) -> tide::Result<String> {
-    let res = "Hello world!".to_owned();
-    Ok(res)
+#[derive(Template)] 
+#[template(path = "index.html")] // using the template in this path, relative
+struct IndexTemplate { 
+    repos: Vec<Repository>
+}
+
+async fn index(req: Request<()>) -> tide::Result {
+    let repos = &Config::global().repo_directory;
+    let paths = fs::read_dir(repos)?;
+    let mut index_template = IndexTemplate{
+        repos:  vec![]
+    }; // TODO replace with map/collect
+    for path in paths {
+        let repo = Repository::open(path?.path())?;
+        index_template.repos.push(repo);
+    }
+    Ok(index_template.into())
+}
+
+#[derive(Template)] 
+#[template(path = "repo.html")] // using the template in this path, relative
+struct RepoHomeTemplate<'a> { 
+    repo: Repository,
+    readme_text: &'a str
+}
+
+async fn repo_home(req: Request<()>) -> tide::Result {
+    let repo_path = Path::new(&Config::global().repo_directory).join(req.param("repo_name")?);
+    // TODO CLEAN PATH! VERY IMPORTANT! DONT FORGET!
+    let repo = Repository::open(repo_path)?;
+    let tmpl = RepoHomeTemplate {
+        repo: repo,
+        readme_text: "Hello world"
+    };
+    Ok(tmpl.into())
 }
 
 const HELP: &str = "\
@@ -33,6 +69,7 @@ FLAGS:
 OPTIONS:
   -c                    Path to config file
 ";
+
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -52,6 +89,9 @@ async fn main() -> Result<(), std::io::Error> {
     tide::log::start();
     let mut app = tide::new();
     app.at("/").get(index);
+    app.at("/:repo_name").get(repo_home);
+    app.at("/:repo_name/log").get(repo_log);
+    // app.at("/:repo_name/:commit/tree").get(repo_log);
     app.listen("127.0.0.1:8081").await?;
     Ok(())
 }
